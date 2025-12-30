e46c78166fe7f52a40fc6b9d343ed4a6c72021c0
ParagD1606 <paragdharamkar2006@gmail.com>
2025-12-30 12:54:13 +0530

    Implement movie search functionality, enhance UI components, and integrate Appwrite for search count tracking

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,67 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, ScrollView, ActivityIndicator, FlatList } from "react-native";
 import "../../global.css"
 import { Link } from "expo-router";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
+import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
 
 export default function Index() {
+
+  const router = useRouter();
+
+  const {data: movies, loading: moviesLoading, error: moviesError} = useFetch(()=> fetchMovies({
+    query: '',
+  }))
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-primary">
-        Welcome to Nativewind!
-      </Text>
-      <Link href="/onboarding" className="mt-4 px-4 py-2 text-light-100 bg-primary rounded">
-        Go to Onboarding
-      </Link>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full" />
+
+      <ScrollView className="flex 1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}> 
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        {moviesLoading ? (
+          <ActivityIndicator size="large" color="#AB8BFF" className="mt-10" />
+        ) : moviesError ? (
+          <Text className="text-red-500 text-center mt-10">
+            {moviesError.message}
+          </Text>
+        ) : (
+          <View className="flex 1 mt-5">
+            <SearchBar 
+              onPress={()=> router.push("/search")}
+              placeholder="Search for movies or TV series"
+            />
+
+            <>
+              <Text className="text-white text-xl font-bold mt-10 mb-5">
+                Latest Movies
+              </Text>
+
+              <FlatList 
+                data={movies}
+                renderItem={({item})=> (
+                  <Text className="text-white text-base mb-3">
+                    {item.title}
+                  </Text>
+                )}
+                keyExtractor={(item)=> item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{ justifyContent: 'flex-start', marginBottom: 10, gap: 20, paddingRight: 5 }}
+                className="mt-2 pb-32"
+                scrollEnabled={false}
+              />
+            </>
+          </View>
+        )
+        }
+
+        
+      </ScrollView>
+
     </View>
   );
 }
---
+++ services/useFetch.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+const useFetch = <T>(fetchFunction: ()=> Promise<T>, autoFetch = true) => {
+    const [data, setData] = useState<T | null>(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<Error | null>(null);
+
+    const fetchData = async () => {
+        try {
+            setLoading(true);
+            const result = await fetchFunction();
+            setData(result);
+            setError(null);
+        } catch(err){
+            setError(err instanceof Error ? err : new Error(String(err)));
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const reset = () => {
+        setData(null);
+        setError(null);
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        if (autoFetch) {
+            fetchData();
+        }
+    }, []);
+
+    return { data, loading, error, refetch: fetchData, reset };
+}
+
+export default useFetch
